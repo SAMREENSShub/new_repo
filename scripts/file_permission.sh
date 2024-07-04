809de1a6fec7c693034c091654db7de182235894
SAMREENSS <samreenss2414@gmail.com>
2024-07-04 07:45:00 +0530

    Revert "dir"
    This reverts commit a1077bf44672d09ba0fb4522796adb9e24f1ef1e.
    
    	deleted:    scripts/count_words.sh
    	deleted:    scripts/file_permission.sh
    deleted:    scripts/list_user_home_dir.sh
    	deleted:    scripts/remain_30file.sh
    	deleted:    scripts/scripts
    	deleted:    scripts/vowel_consonants.sh
    	deleted:    scripts/vowel_grep.sh

--- scripts/file_permission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dir=$1
-for file in "${dir[@]}"
-
-do 
-	echo "setting the permission"
-	chmod -R 777 "$file"
-done
-
--- scripts/list_user_home_dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-awk '{ print $1,$5}' /etc/passwd
-while read home;
-do
-  echo "User: $user, Home Directory: $home"
-done
-
